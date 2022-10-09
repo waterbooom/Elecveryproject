90b14ea01d2741beafa8142a73f950fa1c1be22b
kimbeomsoo <bumsu1027@naver.com>
2022-10-09 14:10:27 +0900

    test update

--- app/src/main/java/com/elecveryproject/MainActivity.java
+++ app/src/main/java/com/elecveryproject/MainActivity.java
@@ -14,13 +14,11 @@ import com.elecveryproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //test test
+        com.elecveryproject.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
